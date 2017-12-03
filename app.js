58e1849102d3003f94b2ff4272768933d42aea67
Brandon Troche <bttroche@gmail.com>
2017-12-03 02:28:29 -0500

    - Installed new dependency - Implemented GIF Speech Command

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var routes       = require('./routes/routes.js');
 var fs           = require("fs");
 var response     = fs.readFileSync("speech/response.json");
 var responseContent = JSON.parse(response);
+const P          = require('bluebird');
+let exec         = require('child_process').exec;
+
+let IMG_COUNT = 10;
+let INTERVAL_MS = 300;
+let FULL_SIZE = false;
 
 function onConnect(responseCode){
     if (responseCode !== 'OK'){
@@ -175,7 +181,7 @@ if (voiceCommand == 'snap'){
             console.dir(response);
             logger.green('snap done. exiting.');
 
-            process.exit(0);
+//            process.exit(0);
 
         })
             .catch((error) => {
@@ -186,7 +192,7 @@ if (voiceCommand == 'snap'){
         });
     
         
-        camera.getLastImage()
+        cam.getLastImage()
             .then((response) => {
 
             // get the first and only item in the response array
@@ -212,8 +218,151 @@ if (voiceCommand == 'snap'){
         });
 
     });
-} else if (voiceCommand == 'snap'){
-    
+} else if (voiceCommand == 'shoot'){
+    const options = process.argv.slice(2).reduce((acc, arg) => {
+
+    let [k, v = true] = arg.split('=');
+    switch (k) {
+
+      case 'frames':
+        v = parseInt(v);
+        if (typeof v === 'number' && v < 20 && v > 0) {
+          IMG_COUNT = v;
+        } 
+        break;
+      case 'interval':
+        v = parseInt(v);
+        if (typeof v === 'number' && v > 500) {
+          INTERVAL_MS = v;
+        }
+        break;
+    }
+
+    acc[k] = v;
+    return acc;
+
+}, {})
+
+if (options.debug === 'true') {
+  logger.setLevel('debug');
+}
+
+if (options.size !== undefined && options.size === 'full') {
+  FULL_SIZE = true;
+}
+
+
+let imgCounter = 0;
+let imageFiles = [];
+
+
+// connect to the camera using PTP-IP
+cam.ipConnect((responseCode) => {
+  if (responseCode === 'OK') {
+    gifLoop();
+  } else {
+    logger.red('Problem connecting: ' + responseCode);
+    process.exit(0);
+  }
+
+});
+
+
+function gifLoop() {
+
+  if (imgCounter < IMG_COUNT) {
+
+    cam.snap()
+    .then((response) => {
+
+      logger.green('snapped img ' + imgCounter);
+      imgCounter++;
+
+      setTimeout(function() {
+        gifLoop();
+      }, INTERVAL_MS);
+
+    })
+    .catch((error) => {
+      logger.red('snap error: ');
+      logger.red(error);
+    });
+
+  } else {
+
+    let objects;
+		if (FULL_SIZE) {
+			objects = cam.getLastImage(IMG_COUNT);
+    } else {
+			objects = cam.getLastThumb(IMG_COUNT);
+    }
+
+    objects.then((response) => {
+
+      logger.green('got ' + response.length + ' thumbs');
+
+      for (let i = 0; i < response.length; i++) {
+
+        let filename = 'gif/gif-img-' + i + '.jpg';
+
+        let obj = response[i].image;
+
+        fs.writeFileSync(filename, obj, {encoding: 'binary'});
+
+        logger.green('saved ' + filename);
+        imageFiles[i] = filename;
+      }
+
+      // generate gif
+      generateGif(imageFiles);
+
+    })
+    .catch((error) => {
+      logger.red('getLastImage error');
+      logger.red(error);
+    });
+
+  }
+
+}
+
+function generateGif(files) {
+
+  logger.cyan('generating gif...');
+  console.dir(options);
+
+  let inputString = (() => {
+    let str = '';
+    for (let name of files) {
+      str += (name + ' ');
+    }
+    return str;
+  })();
+
+  let convert = exec('convert -loop 0 -delay ' + (INTERVAL_MS/10) + ' ' + inputString + 'gif/img.gif');
+
+  convert.stdout.on('data', data => {
+    console.log( `convert: ${data}` );
+  });
+
+  convert.stderr.on('data', data => {
+    console.log( `convert: ${data}` );
+  });
+
+  convert.on('close', code => {
+    logger.cyan( 'convert exited with code ' + code );
+    if (code === 0) {
+      logger.green('done');
+      exec('qlmanage -p gif/img.gif >& /dev/null');
+    } else {
+      logger.red('ImageMagick had a problem');
+      logger.red('Ensure it is installed on your machine');
+    }
+		process.exit(0);
+  });
+
+}
+
 }
 
 //app.listen(port);
